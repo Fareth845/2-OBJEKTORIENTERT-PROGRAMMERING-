2208e8dff6d0b9dcf485d14f5914deeaa14911e2
Fareth845 <148755139+Fareth845@users.noreply.github.com>
2023-10-30 11:01:03 +0100

    Update planeter.py

--- planeter.py
+++ planeter.py
@@ -1,21 +1,19 @@
+import math as m
 
 class Planet:
     " En klasse for å beskrive en planet"
-    def __init__(self,navn:str,solavstand:float,radius:float, antallringer = 0) -> None:
-
+    def __init__(self,navn:str,solavstand:float,radius:float) -> None:
         self.navn=navn
         self.solavstand=solavstand
         self.radius = radius
-        self.antallringer = antallringer
 
 jorden = Planet( "Jorden", 152, 6371, 0)
 jupiter = Planet("Jupiter", 778, 69911, 0)
 mars = Planet("Mars", 227.9, 3389.5, 0)
 
-print(jorden.antallringer)
+print(jorden.navn)
 # Oppgave 1: 
 # Lag et objekt for Mars, Jupiter og Jorda, der du lagrer informasjon om navn, solavstand og radius. Lagre disse objektene i egne variabler
-
 
 # Hva skjer om du skriver print(Jorda)?
 # Hva skjer om du skriver print(Jorda.navn)?
